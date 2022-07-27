bbe0fdb8d2f2986c1dd73a6feb699c852a8119d4
thaovyntr <52333253+thaovyntr@users.noreply.github.com>
2022-07-27 15:58:34 -0700

    Adding clickData

--- botanyapp.py
+++ botanyapp.py
@@ -1,10 +1,14 @@
 import dash
 from dash import dcc
 from dash import html
+from dash import Input, Output
 import plotly.express as px
 import pandas as pd
+import json
+from dash.exceptions import PreventUpdate
 
 app = dash.Dash(__name__)
+
 
 #load data into pandas
 df = pd.read_csv(r'/Users/a1/Downloads/PortalFiles/PortalData.csv', 
@@ -12,7 +16,7 @@ df = pd.read_csv(r'/Users/a1/Downloads/PortalFiles/PortalData.csv',
                           "Class","Order","Family","Species",
                           "Continent","Country"])
 
-#creating map
+#create map
 fig = px.scatter_geo(df,
                      projection = 'orthographic',
                      lat='latitude1',
@@ -24,19 +28,51 @@ fig = px.scatter_geo(df,
                                  'Family',
                                  'Species',
                                  'Country'])
-fig.update_layout(title = 'Map of plant species around the world', title_x=0.5)
+#fig.update_layout(title = 'Map of plant species around the world', title_x=0.5)
 fig.show()
 
-#Dash layout
-app.layout = html.Div(children=[
-    html.H1(children='Map of plant species around the world'),
-    html.Div(children='This data was provided by California Academy of Science.'),
 
-    dcc.Graph(
-        id='botany-map',
-        figure=fig
-    )
-])
+
+#Dash layout
+styles = {
+    'pre': {
+        'border': 'thin lightgrey solid',
+        'overflowX': 'scroll'
+    }
+}
+
+app.layout = html.Div(
+    children=[
+        html.H1(children='Map of plant species around the world'),
+
+        html.Span("Hover over a plant on the map to see its information."),
+        html.Br(),
+
+        dcc.Graph(
+            id='botany-map',
+            figure=fig
+        ),
+
+
+        html.Div([
+            dcc.Markdown("""
+                **Click Data**
+
+                Click on points in the graph.
+                """),
+            html.Pre(id='click-data', style=styles['pre']),
+        ], className='three columns'),
+    ]
+
+)               
+
+@app.callback(
+    Output('click-data', 'children'),
+    Input('botany-map', 'clickData'))
+def display_click_data(clickData):
+    return json.dumps(clickData, indent=2)   
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
